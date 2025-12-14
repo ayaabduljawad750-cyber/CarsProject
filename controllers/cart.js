de0fb6d5d63201db7984529b85c3234914e47840
ayaabduljawad750-cyber <ayaabduljawad750@gmail.com>
2025-12-14 15:12:27 +0200

    update

--- controllers/cart.js
+++ controllers/cart.js
@@ -38,7 +38,7 @@ const addToCart = catchError(async (req, res, next) => {
     );
 
     if (itemIndex > -1) {
-    //  const newQty = cart.items[itemIndex].quantity + qty;
+     const newQty = 1
 
       if (newQty > product.stock) {
         return next(
